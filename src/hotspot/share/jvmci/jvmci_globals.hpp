c1c386a23e3f20508a0a5d2ef7ea462d2e457ca6
Vladimir Kozlov <kvn@openjdk.org>
2020-03-17 12:42:48 -0700

    8237045: JVM uses excessive memory with -XX:+EnableJVMCI -XX:JVMCICounterSize=2147483648
    Limit JVMCICounterSize flag's value range to 1M
    
    Reviewed-by: thartmann, redestad

--- src/hotspot/share/jvmci/jvmci_globals.hpp
+++ src/hotspot/share/jvmci/jvmci_globals.hpp
@@ -76,7 +76,8 @@ class fileStream;
                                                                             \
   NOT_COMPILER2(product(intx, MaxVectorSize, 64,                            \
           "Max vector size in bytes, "                                      \
-          "actual size could be less depending on elements type"))          \
+          "actual size could be less depending on elements type")           \
+          range(0, max_jint))                                               \
                                                                             \
   NOT_COMPILER2(product(bool, ReduceInitialCardMarks, true,                 \
           "Defer write barriers of young objects"))                         \
@@ -85,10 +86,11 @@ class fileStream;
           "Trace level for JVMCI: "                                         \
           "1 means emit a message for each CompilerToVM call,"              \
           "levels greater than 1 provide progressively greater detail")     \
+          range(0, 6)                                                       \
                                                                             \
   experimental(intx, JVMCICounterSize, 0,                                   \
           "Reserved size for benchmark counters")                           \
-          range(0, max_jint)                                                \
+          range(0, 1000000)                                                 \
                                                                             \
   experimental(bool, JVMCICountersExcludeCompiler, true,                    \
           "Exclude JVMCI compiler threads from benchmark counters")         \
@@ -98,6 +100,7 @@ class fileStream;
                                                                             \
   experimental(intx, JVMCINMethodSizeLimit, (80*K)*wordSize,                \
           "Maximum size of a compiled method.")                             \
+          range(0, max_jint)                                                \
                                                                             \
   experimental(intx, MethodProfileWidth, 0,                                 \
           "Number of methods to record in call profile")                    \
