e3693bbc805494d28fe863ca697c05c7f04f4c8b
kvergizova <katya@azul.com>
2020-11-03 17:29:11 +0300

    Backport 7b82266a159ce363708e347aba7e1b0d38206b48

--- src/java.base/share/classes/module-info.java
+++ src/java.base/share/classes/module-info.java
@@ -208,6 +208,8 @@ module java.base {
         java.management,
         jdk.management.agent,
         jdk.internal.jvmstat;
+    exports jdk.internal.platform to
+        jdk.management;
     exports jdk.internal.ref to
         java.desktop;
     exports jdk.internal.reflect to
