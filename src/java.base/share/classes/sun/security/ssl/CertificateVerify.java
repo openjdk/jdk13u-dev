89bc0dcc3193ebf22d1cfc80344f1ec41c401eda
Olga Mikhaltsova <olga@azul.com>
2020-11-17 20:42:06 +0300

    Backport 316140ff92af7ac1aadb74de9cd37a5f3c412406

--- src/java.base/share/classes/sun/security/ssl/CertificateVerify.java
+++ src/java.base/share/classes/sun/security/ssl/CertificateVerify.java
@@ -586,6 +586,7 @@ final class CertificateVerify {
             // This happens in client side only.
             ClientHandshakeContext chc = (ClientHandshakeContext)context;
             this.signatureScheme = SignatureScheme.getPreferableAlgorithm(
+                    chc.algorithmConstraints,
                     chc.peerRequestedSignatureSchemes,
                     x509Possession,
                     chc.negotiatedProtocol);
@@ -898,6 +899,7 @@ final class CertificateVerify {
             super(context);
 
             this.signatureScheme = SignatureScheme.getPreferableAlgorithm(
+                    context.algorithmConstraints,
                     context.peerRequestedSignatureSchemes,
                     x509Possession,
                     context.negotiatedProtocol);
