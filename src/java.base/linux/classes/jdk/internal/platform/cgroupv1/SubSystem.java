d6c3d09b3bfce0dffd441d8835bed659afa3d562
kvergizova <katya@azul.com>
2020-12-04 12:49:21 +0300

    Backport 0187567704d79ecf394d4cb656d0ba4c886351f1

--- src/java.base/linux/classes/jdk/internal/platform/cgroupv1/SubSystem.java
+++ src/java.base/linux/classes/jdk/internal/platform/cgroupv1/SubSystem.java
@@ -266,6 +266,7 @@ public class SubSystem {
     public static class MemorySubSystem extends SubSystem {
 
         private boolean hierarchical;
+        private boolean swapenabled;
 
         public MemorySubSystem(String root, String mountPoint) {
             super(root, mountPoint);
@@ -279,5 +280,12 @@ public class SubSystem {
             this.hierarchical = hierarchical;
         }
 
+        boolean isSwapEnabled() {
+            return swapenabled;
+        }
+
+        void setSwapEnabled(boolean swapenabled) {
+            this.swapenabled = swapenabled;
+        }
     }
 }
