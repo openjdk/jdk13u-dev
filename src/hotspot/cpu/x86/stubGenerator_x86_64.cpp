496b66fe237575a3083bc417c8cf79b7dce4c97b
Ekaterina Vergizova <evergizova@openjdk.org>
2020-12-11 11:19:20 +0000

    8243114: Implement montgomery{Multiply,Square}intrinsics on Windows
    Reviewed-by: yan
    Backport-of: 47e465cf1bc5e78fe0b5ee1b574fdfa2385d7f2c

--- src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
@@ -6065,7 +6065,6 @@ address generate_avx_ghash_processBlocks() {
     if (UseMulAddIntrinsic) {
       StubRoutines::_mulAdd = generate_mulAdd();
     }
-#ifndef _WINDOWS
     if (UseMontgomeryMultiplyIntrinsic) {
       StubRoutines::_montgomeryMultiply
         = CAST_FROM_FN_PTR(address, SharedRuntime::montgomery_multiply);
@@ -6074,7 +6073,6 @@ address generate_avx_ghash_processBlocks() {
       StubRoutines::_montgomerySquare
         = CAST_FROM_FN_PTR(address, SharedRuntime::montgomery_square);
     }
-#endif // WINDOWS
 #endif // COMPILER2
 
     if (UseVectorizedMismatchIntrinsic) {
