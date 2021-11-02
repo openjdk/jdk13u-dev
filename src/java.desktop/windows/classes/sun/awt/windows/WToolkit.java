89a74fa4b619c15bc9f86cc9ee80d592919e530c
Yuri Nesterenko <yan@azul.com>
2021-11-02 16:36:34 +0300

    Backport 973255c469d794afe8ee74b24ddb5048bfcaadf7

--- src/java.desktop/windows/classes/sun/awt/windows/WToolkit.java
+++ src/java.desktop/windows/classes/sun/awt/windows/WToolkit.java
@@ -1185,7 +1185,7 @@ public final class WToolkit extends SunToolkit implements Runnable {
     ///////////////////////////////////////////////////////////////////////////
 
     @Override
-    public native boolean syncNativeQueue(final long timeout);
+    public native boolean syncNativeQueue(long timeout);
 
     @Override
     public boolean isDesktopSupported() {
