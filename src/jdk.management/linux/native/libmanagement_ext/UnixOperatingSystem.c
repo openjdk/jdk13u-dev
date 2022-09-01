97c75f0f022ed5333c44d7700e230fccd0e5209f
Roman Marchenko <rmarchenko@azul.com>
2022-09-01 12:20:30 +0000

    8268361: Fix the infinite loop in next_line
    Backport-of: 72672277e4dddf8e72f1c705cd5f57de40745635

--- src/jdk.management/linux/native/libmanagement_ext/UnixOperatingSystem.c
+++ src/jdk.management/linux/native/libmanagement_ext/UnixOperatingSystem.c
@@ -60,8 +60,13 @@ static struct perfbuf {
 
 #define DEC_64 "%"SCNd64
 
-static void next_line(FILE *f) {
-    while (fgetc(f) != '\n');
+static int next_line(FILE *f) {
+    int c;
+    do {
+        c = fgetc(f);
+    } while (c != '\n' && c != EOF);
+
+    return c;
 }
 
 /**
@@ -90,7 +95,10 @@ static int get_totalticks(int which, ticks *pticks) {
            &iowTicks, &irqTicks, &sirqTicks);
 
     // Move to next line
-    next_line(fh);
+    if (next_line(fh) == EOF) {
+        fclose(fh);
+        return -2;
+    }
 
     //find the line for requested cpu faster to just iterate linefeeds?
     if (which != -1) {
@@ -103,7 +111,10 @@ static int get_totalticks(int which, ticks *pticks) {
                 fclose(fh);
                 return -2;
             }
-            next_line(fh);
+            if (next_line(fh) == EOF) {
+                fclose(fh);
+                return -2;
+            }
         }
         n = fscanf(fh, "cpu%*d " DEC_64 " " DEC_64 " " DEC_64 " " DEC_64 " "
                        DEC_64 " " DEC_64 " " DEC_64 "\n",
